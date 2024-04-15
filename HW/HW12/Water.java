a54442bd38cd17cb9b3dc4682b7b177a2c73fef3
rnshim <ryanshim77@gmail.com>
2024-04-14 22:57:44 -0700

    hw12

---
+++ HW/HW12/Water.java
@@ -0,0 +1,7 @@
+package HW.HW12;
+
+public class Water {
+    public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
+        return 0;
+     }
+}
---
+++ HW/HW12/Water.py
@@ -0,0 +1,5 @@
+from typing import List
+
+
+def minCostToSupplyWater(self, n: int, wells: List[int], pipes: List[List[int]]) -> int:
+    pass
